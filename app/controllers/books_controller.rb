df8bbda8e3ec30cba2fb7a71150213f7e87e7105
takaponmonogatari <taka11hiro26@gmail.com>
2021-04-27 10:45:24 +0000

    [Update] ユーザー認証機能の作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+
+ def index
+  @book = Book.new
+  @books = Book.all
+ end
+
+ def create
+  @book = Book.new(book_params)
+  @book.user_id = current_user.id
+  @book.save
+  redirect_to books_path
+ end
+
+ def show
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
+ def destroy
+ end
+
+ private
+
+ def book_params
+  params.require(:book).permit(:title, :body, :user_id)
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => "homes#index"
   get 'homes/about'
+  resources:users
+  resources:books
 end
